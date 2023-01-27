602138256c266e1ab5340a3e56ba57c5c5d02c15
Josef Taylor <josef.taylor@gmail.com>
2023-01-27 16:21:48 -0500

    Got while loops working, detect keyboard input to switch context.

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,62 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// While running (run forever)
+(LOOP)
+  // Initialize the i counter
+  // @SCREEN
+  // D=M
+  @i
+  M=0
+  // read the contents of keyboard memory
+  @24576
+  D=M
+  @whileBlack
+  D;JGT
+  // if no key is pressed, write zeros to every register of the screen memory
+  (whileWhite)
+    @i
+    D=M
+    @10
+    D=D-A
+    @endWhileWhite
+    D;JGT
+      @SCREEN
+      D=A
+      @i
+      D=D+M
+      @screenPoint
+      M=D
+      @M      
+      M=0
+    @i
+    M=M+1
+    @whileWhite
+    0;JMP
+  (endWhileWhite)
+  @LOOP
+  0;JMP
+  (whileBlack)
+  // if a key is pressed (keyboard != 0), write 1 to every register of the screen memory
+    @i
+    D=M
+    @10
+    D=D-A
+    @endWhileBlack
+    D;JGT
+      @SCREEN
+      D=A
+      @i
+      D=D+M
+      @screenPoint
+      M=D
+      @M
+    @i
+    M=M+1
+    @whileBlack
+    0;JMP
+  (endWhileBlack)
+// end while
+  @LOOP
+  0;JMP
+(END)
