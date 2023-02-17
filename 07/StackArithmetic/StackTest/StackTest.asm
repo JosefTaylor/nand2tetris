5bc8148d72404c611bfa4419bfea46ebe0491af6
Josef Taylor <josef.taylor@gmail.com>
2023-02-16 22:35:53 -0500

    got jump lines (program memory) working, but something isn't right.

--- 07/StackArithmetic/StackTest/StackTest.asm
+++ 07/StackArithmetic/StackTest/StackTest.asm
@@ -1,45 +1,11 @@
-@17 // push constant x
+@17 // push constant 17
 D=A
 @SP
 A=M
 M=D
 @SP
 M=M+1
-@17 // push constant x
-D=A
-@SP
-A=M
-M=D
-@SP
-M=M+1
-@SP // check equality of two numbers from the stack
-AM=M-1
-D=M
-@SP
-AM=M-1
-D=D-M
-@EQUAL
-D;JEQ
-@SP
-A=M
-M=0
-@END
-0;JMP
-(EQUAL)
-@SP
-A=M
-M=-1
-(END)
-@SP
-M=M+1
-@17 // push constant x
-D=A
-@SP
-A=M
-M=D
-@SP
-M=M+1
-@16 // push constant x
+@17 // push constant 17
 D=A
 @SP
 A=M
@@ -52,28 +18,26 @@ D=M
 @SP
 AM=M-1
 D=D-M
-@EQUAL
+@28 // EQUAL
 D;JEQ
 @SP
 A=M
 M=0
-@END
+@31 // END
 0;JMP
-(EQUAL)
-@SP
+@SP // EQUAL
 A=M
 M=-1
-(END)
-@SP
+@SP // END
 M=M+1
-@16 // push constant x
+@17 // push constant 17
 D=A
 @SP
 A=M
 M=D
 @SP
 M=M+1
-@17 // push constant x
+@16 // push constant 16
 D=A
 @SP
 A=M
@@ -86,131 +50,332 @@ D=M
 @SP
 AM=M-1
 D=D-M
-@EQUAL
+@60 // EQUAL
 D;JEQ
 @SP
 A=M
 M=0
-@END
+@63 // END
 0;JMP
-(EQUAL)
-@SP
+@SP // EQUAL
 A=M
 M=-1
-(END)
-@SP
+@SP // END
 M=M+1
-@892 // push constant x
+@16 // push constant 16
 D=A
 @SP
 A=M
 M=D
 @SP
 M=M+1
-@891 // push constant x
+@17 // push constant 17
 D=A
 @SP
 A=M
 M=D
 @SP
 M=M+1
-@SP// check equality of two numbers from the stack
+@SP // check equality of two numbers from the stack
 AM=M-1
 D=M
 @SP
 AM=M-1
 D=D-M
-@LESS_THAN
-D;JLT
+@92 // EQUAL
+D;JEQ
 @SP
 A=M
 M=0
-@END
+@95 // END
 0;JMP
-(LESS_THAN)
-@SP
+@SP // EQUAL
 A=M
 M=-1
-(END)
-@SP
+@SP // END
 M=M+1
-@891 // push constant x
+@892 // push constant 892
 D=A
 @SP
 A=M
 M=D
 @SP
 M=M+1
-@892 // push constant x
+@891 // push constant 891
 D=A
 @SP
 A=M
 M=D
 @SP
 M=M+1
-@SP// check equality of two numbers from the stack
+@SP // check that the number at SP-2 < number at SP-1
 AM=M-1
 D=M
 @SP
 AM=M-1
-D=D-M
-@LESS_THAN
+D=M-D
+@124 // LESS_THAN
 D;JLT
 @SP
 A=M
 M=0
-@END
+@127 // END
 0;JMP
-(LESS_THAN)
-@SP
+@SP // LESS_THAN
 A=M
 M=-1
-(END)
-@SP
+@SP // END
 M=M+1
-@891 // push constant x
+@891 // push constant 891
 D=A
 @SP
 A=M
 M=D
 @SP
 M=M+1
-@891 // push constant x
+@892 // push constant 892
 D=A
 @SP
 A=M
 M=D
 @SP
 M=M+1
-@SP// check equality of two numbers from the stack
+@SP // check that the number at SP-2 < number at SP-1
 AM=M-1
 D=M
 @SP
 AM=M-1
-D=D-M
-@LESS_THAN
+D=M-D
+@156 // LESS_THAN
 D;JLT
 @SP
 A=M
 M=0
-@END
+@159 // END
 0;JMP
-(LESS_THAN)
-@SP
+@SP // LESS_THAN
 A=M
 M=-1
-(END)
-@SP
+@SP // END
 M=M+1
-@32767 // push constant x
+@891 // push constant 891
 D=A
 @SP
 A=M
 M=D
 @SP
 M=M+1
-@32766 // push constant x
+@891 // push constant 891
 D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP // check that the number at SP-2 < number at SP-1
+AM=M-1
+D=M
+@SP
+AM=M-1
+D=M-D
+@188 // LESS_THAN
+D;JLT
+@SP
+A=M
+M=0
+@191 // END
+0;JMP
+@SP // LESS_THAN
+A=M
+M=-1
+@SP // END
+M=M+1
+@32767 // push constant 32767
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@32766 // push constant 32766
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP // check that the number at SP-2 > number at SP-1
+AM=M-1
+D=M
+@SP
+AM=M-1
+D=M-D
+@220 // GREATER_THAN
+D;JGT
+@SP
+A=M
+M=0
+@223 //END
+0;JMP
+@SP // GREATER_THAN
+A=M
+M=-1
+@SP // END
+M=M+1
+@32766 // push constant 32766
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@32767 // push constant 32767
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP // check that the number at SP-2 > number at SP-1
+AM=M-1
+D=M
+@SP
+AM=M-1
+D=M-D
+@252 // GREATER_THAN
+D;JGT
+@SP
+A=M
+M=0
+@255 //END
+0;JMP
+@SP // GREATER_THAN
+A=M
+M=-1
+@SP // END
+M=M+1
+@32766 // push constant 32766
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@32766 // push constant 32766
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP // check that the number at SP-2 > number at SP-1
+AM=M-1
+D=M
+@SP
+AM=M-1
+D=M-D
+@284 // GREATER_THAN
+D;JGT
+@SP
+A=M
+M=0
+@287 //END
+0;JMP
+@SP // GREATER_THAN
+A=M
+M=-1
+@SP // END
+M=M+1
+@57 // push constant 57
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@31 // push constant 31
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@53 // push constant 53
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP // add two numbers from the stack
+AM=M-1
+D=M
+@SP
+AM=M-1
+D=D+M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@112 // push constant 112
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP // subtract two numbers from the stack
+AM=M-1
+D=M
+@SP
+AM=M-1
+D=M-D
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP // negate the last number from the stack
+AM=M-1
+D=-M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP // AND two numbers from the stack
+AM=M-1
+D=M
+@SP
+AM=M-1
+D=D&M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@82 // push constant 82
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP // OR two numbers from the stack
+AM=M-1
+D=M
+@SP
+AM=M-1
+D=D|M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP // NOT the last number from the stack
+AM=M-1
+D=!M
 @SP
 A=M
 M=D
--- 07/vm-translator.py
+++ 07/vm-translator.py
@@ -4,69 +4,6 @@ import re
 
 
 def main():
-    commands = {
-        "push": {
-            "constant": "@{} // push constant x\n"
-            + "D=A\n"
-            + "@SP\n"
-            + "A=M\n"
-            + "M=D\n"
-            + "@SP\n"
-            + "M=M+1\n"
-        },
-        "add": "@SP // add two numbers from the stack\n"
-        + "AM=M-1\n"
-        + "D=M\n"
-        + "@SP\n"
-        + "AM=M-1\n"
-        + "D=D+M\n"
-        + "@SP\n"
-        + "A=M\n"
-        + "M=D\n"
-        + "@SP\n"
-        + "M=M+1\n",
-        "eq": "@SP // check equality of two numbers from the stack\n"
-        + "AM=M-1\n"
-        + "D=M\n"
-        + "@SP\n"
-        + "AM=M-1\n"
-        + "D=D-M\n"
-        + "@EQUAL\n"
-        + "D;JEQ\n"
-        + "@SP\n"
-        + "A=M\n"
-        + "M=0\n"
-        + "@END\n"
-        + "0;JMP\n"
-        + "(EQUAL)\n"
-        + "@SP\n"
-        + "A=M\n"
-        + "M=-1\n"
-        + "(END)\n"
-        + "@SP\n"
-        + "M=M+1\n",
-        "lt": "@SP// check equality of two numbers from the stack\n"
-        + "AM=M-1\n"
-        + "D=M\n"
-        + "@SP\n"
-        + "AM=M-1\n"
-        + "D=D-M\n"
-        + "@LESS_THAN\n"
-        + "D;JLT\n"
-        + "@SP\n"
-        + "A=M\n"
-        + "M=0\n"
-        + "@END\n"
-        + "0;JMP\n"
-        + "(LESS_THAN)\n"
-        + "@SP\n"
-        + "A=M\n"
-        + "M=-1\n"
-        + "(END)\n"
-        + "@SP\n"
-        + "M=M+1\n",
-    }
-
     parser = argparse.ArgumentParser()
 
     parser.add_argument("path")
@@ -78,17 +15,173 @@ def main():
     s = open(source)
     t = open(source.with_suffix(".asm"), "w")
 
+    line_num = 1
+
     for line in s:
         line = line.split("//")[0].strip()
         tokens = line.split(" ")
         if len(tokens) > 0 and tokens[0]:
-            command = commands
-            i = 0
-            while type(command) is dict:
-                command = command[tokens[i]]
-                i += 1
+            code_lines = command(tokens, line_num)
+            line_num += len(code_lines)
+            t.writelines(code_lines)
 
-            t.write(command.format(*tokens[i:]))
+    s.close()
+    t.close()
+
+
+def command(tokens, line):
+    commands = {
+        "push": {
+            "constant": [
+                f"@{tokens[-1]} // push constant {tokens[-1]}\n",
+                f"D=A\n",
+                f"@SP\n",
+                f"A=M\n",
+                f"M=D\n",
+                f"@SP\n",
+                f"M=M+1\n",
+            ]
+        },
+        "add": [
+            "@SP // add two numbers from the stack\n",
+            "AM=M-1\n",
+            "D=M\n",
+            "@SP\n",
+            "AM=M-1\n",
+            "D=D+M\n",
+            "@SP\n",
+            "A=M\n",
+            "M=D\n",
+            "@SP\n",
+            "M=M+1\n",
+        ],
+        "sub": [
+            "@SP // subtract two numbers from the stack\n",
+            "AM=M-1\n",
+            "D=M\n",
+            "@SP\n",
+            "AM=M-1\n",
+            "D=M-D\n",
+            "@SP\n",
+            "A=M\n",
+            "M=D\n",
+            "@SP\n",
+            "M=M+1\n",
+        ],
+        "and": [
+            "@SP // AND two numbers from the stack\n",
+            "AM=M-1\n",
+            "D=M\n",
+            "@SP\n",
+            "AM=M-1\n",
+            "D=D&M\n",
+            "@SP\n",
+            "A=M\n",
+            "M=D\n",
+            "@SP\n",
+            "M=M+1\n",
+        ],
+        "or": [
+            "@SP // OR two numbers from the stack\n",
+            "AM=M-1\n",
+            "D=M\n",
+            "@SP\n",
+            "AM=M-1\n",
+            "D=D|M\n",
+            "@SP\n",
+            "A=M\n",
+            "M=D\n",
+            "@SP\n",
+            "M=M+1\n",
+        ],
+        "not": [
+            "@SP // NOT the last number from the stack\n",
+            "AM=M-1\n",
+            "D=!M\n",
+            "@SP\n",
+            "A=M\n",
+            "M=D\n",
+            "@SP\n",
+            "M=M+1\n",
+        ],
+        "neg": [
+            "@SP // negate the last number from the stack\n",
+            "AM=M-1\n",
+            "D=-M\n",
+            "@SP\n",
+            "A=M\n",
+            "M=D\n",
+            "@SP\n",
+            "M=M+1\n",
+        ],
+        "eq": [
+            f"@SP // check equality of two numbers from the stack\n",
+            f"AM=M-1\n",
+            f"D=M\n",
+            f"@SP\n",
+            f"AM=M-1\n",
+            f"D=D-M\n",
+            f"@{line+13} // EQUAL\n",
+            f"D;JEQ\n",
+            f"@SP\n",
+            f"A=M\n",
+            f"M=0\n",
+            f"@{line+16} // END\n",
+            f"0;JMP\n",
+            f"@SP // EQUAL\n",
+            f"A=M\n",
+            f"M=-1\n",
+            f"@SP // END\n",
+            f"M=M+1\n",
+        ],
+        "lt": [
+            f"@SP // check that the number at SP-2 < number at SP-1\n",
+            f"AM=M-1\n",
+            f"D=M\n",
+            f"@SP\n",
+            f"AM=M-1\n",
+            f"D=M-D\n",
+            f"@{line+13} // LESS_THAN\n",
+            f"D;JLT\n",
+            f"@SP\n",
+            f"A=M\n",
+            f"M=0\n",
+            f"@{line+16} // END\n",
+            f"0;JMP\n",
+            f"@SP // LESS_THAN\n",
+            f"A=M\n",
+            f"M=-1\n",
+            f"@SP // END\n",
+            f"M=M+1\n",
+        ],
+        "gt": [
+            f"@SP // check that the number at SP-2 > number at SP-1\n",
+            f"AM=M-1\n",
+            f"D=M\n",
+            f"@SP\n",
+            f"AM=M-1\n",
+            f"D=M-D\n",
+            f"@{line+13} // GREATER_THAN\n",
+            f"D;JGT\n",
+            f"@SP\n",
+            f"A=M\n",
+            f"M=0\n",
+            f"@{line+16} //END\n",
+            f"0;JMP\n",
+            f"@SP // GREATER_THAN\n",
+            f"A=M\n",
+            f"M=-1\n",
+            f"@SP // END\n",
+            f"M=M+1\n",
+        ],
+    }
+
+    command = commands
+    i = 0
+    while type(command) is dict:
+        command = command[tokens[i]]
+        i += 1
+    return command
 
 
 main()
